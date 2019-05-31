cf738a7c57baa48735e45c34e55a67f22a94061e
yuma116 <yoshimoto.yuma276@mail.kyutech.jp>
2019-05-31 20:43:37 +0900

    3

--- 03_190531/velocity_key_c.py
+++ 03_190531/velocity_key_c.py
@@ -1,8 +1,16 @@
-
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import roslib
 import cv2
 
-image = cv2.imread(roslib.packages.get_pkg_dir('lecture_pkg') + '/files/InputKey.png')
-cv2.imshow('InputWindow',image)
-while not rospy.is_shutdown:
-  key = cv2.waitKey(1)
-  rospy.loginfo(key)
+if __name__ == '__main__':
+  rospy.init_node('velocity_key_c')
+  image = cv2.imread(roslib.packages.get_pkg_dir('lecture_pkg') + '/files/InputKey.png')
+  cv2.imshow('InputWindow',image)
+  r = rospy.Rate(10)
+  while not rospy.is_shutdown():
+    key = cv2.waitKey(1)
+    print(key)
+    rospy.loginfo(key)
+    r.sleep()
