cb11152755d8d8eb200998fc36301780ce7b312a
yuma116 <yoshimoto.yuma276@mail.kyutech.jp>
2019-06-14 19:23:46 +0900

    7

--- setup/setup1and2.sh
+++ setup/setup1and2.sh
@@ -1,9 +1,10 @@
 
-
+echo -e "\n\n\n"
 echo ==========================================
 echo " ROSパッケージインストール "
 echo ==========================================
 sudo apt -y install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation ros-kinetic-interactive-markers
+sudo apt -y install ros-kinetic-navigation-stage
 
 
 echo -e "\n\n\n"
